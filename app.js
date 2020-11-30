c534931b854e42d05066d7c81328d7b33b263cee
rahulbandodkar <59685264+rahull04@users.noreply.github.com>
2020-11-30 14:46:05 +0530

    App

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const prettier = require("prettier");
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const person = [
+    {id:'1',question:'What is the capital of India ?',answer:'Delhi',op1:'Maharashtra',op2:'Panjim',op3:'Delhi',op4:'Kerala'},
+    {id:'2',question:'What is the capital of Maharashtra ?',answer:'Mumbai',op1:'Goa',op2:'Panjim',op3:'Delhi',op4:'Mumbai'},
+    {id:'3',question:'What is the capital of Goa ?',answer:'Panjim',op1:'Maharashtra',op2:'Panjim',op3:'Delhi',op4:'Kerala'},
+    {id:'4',question:'What is the capital of Karnataka ?',answer:'Banglore',op1:'Banglore',op2:'Panjim',op3:'Delhi',op4:'Mumbai'},
+]
+
+app.get('/',(req,res)=>{
+    const data = prettier.format(JSON.stringify(person),{ semi: false, parser: "json" })
+    res.send(data);
+})
+
+app.listen(port, hostname, () => {
+  console.log('Server running at port '+port);
+});
