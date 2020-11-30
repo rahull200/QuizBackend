5dcdcc24184a8f0087c0faca954bebf7aaa8d2b9
rahulbandodkar <59685264+rahull04@users.noreply.github.com>
2020-11-30 19:19:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const prettier = require("prettier");
 
-const port = 3000;
+const port = 5000;
 
 const person = [
     {id:'1',question:'What is the capital of India ?',answer:'Delhi',op1:'Maharashtra',op2:'Panjim',op3:'Delhi',op4:'Kerala'},
@@ -11,11 +11,11 @@ const person = [
     {id:'4',question:'What is the capital of Karnataka ?',answer:'Banglore',op1:'Banglore',op2:'Panjim',op3:'Delhi',op4:'Mumbai'},
 ]
 
-app.get('https://secure-badlands-08499.herokuapp.com/',(req,res)=>{
+app.get('/',(req,res)=>{
     const data = prettier.format(JSON.stringify(person),{ semi: false, parser: "json" })
     res.send(data);
 })
 
-app.listen(port,function(){
+app.listen(process.env.PORT || port,function(){
 	console.log("Server running at port 5000");
 });
